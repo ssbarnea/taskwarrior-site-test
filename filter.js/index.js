8a891a4921ad984ea1c1f8d713908bd4e343a39b
Bruno Vernay <brunovern.a@gmail.com>
2016-03-05 22:27:41 +0100

    Update index.js

--- filter.js/index.js
+++ filter.js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-  initSliders();
 
   //NOTE: To append in different container
   var appendToContainer = function(htmlele, record){
@@ -46,41 +45,11 @@ $(document).ready(function(){
 //  FJS.addCriteria({field: 'runtime', ele: '#runtime_filter', type: 'range'});
   FJS.addCriteria({field: 'category', ele: '#genre_category input:checkbox'});
 
-  /*
-   * Add multiple criterial.
-    FJS.addCriteria([
-      {field: 'genre', ele: '#genre_criteria input:checkbox'},
-      {field: 'year', ele: '#year_filter', type: 'range'}
-    ])
-  */
+
 
   window.FJS = FJS;
 });
 
-function initSliders(){
-  $("#rating_slider").slider({
-    min: 8,
-    max: 10,
-    values:[8, 10],
-    step: 0.1,
-    range:true,
-    slide: function( event, ui ) {
-      $("#rating_range_label" ).html(ui.values[ 0 ] + ' - ' + ui.values[ 1 ]);
-      $('#rating_filter').val(ui.values[0] + '-' + ui.values[1]).trigger('change');
-    }
-  });
-
-  $("#runtime_slider").slider({
-    min: 50,
-    max: 250,
-    values:[0, 250],
-    step: 10,
-    range:true,
-    slide: function( event, ui ) {
-      $("#runtime_range_label" ).html(ui.values[ 0 ] + ' mins. - ' + ui.values[ 1 ] + ' mins.');
-      $('#runtime_filter').val(ui.values[0] + '-' + ui.values[1]).trigger('change');
-    }
-  });
 
   $('#category_criteria :checkbox').prop('checked', true);
   $('#all_category').on('click', function(){
