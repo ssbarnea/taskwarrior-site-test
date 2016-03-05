aa900c8e18520568b012497e0048154c3fe50e4e
Bruno Vernay <brunovern.a@gmail.com>
2016-03-05 23:21:35 +0100

    Update index.js

--- filter.js/index.js
+++ filter.js/index.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 
   initCriteria();
 
-
-
   //NOTE: To append in different container
   var appendToContainer = function(htmlele, record){
     console.log(record)
@@ -21,32 +19,12 @@ $(document).ready(function(){
     //appendToContainer: appendToContainer
   });
 
-  FJS.addCallback('beforeAddRecords', function(){
-    if(this.recordsCount >= 450){
-      this.stopStreaming();
-    }
-  });
 
-  FJS.addCallback('afterAddRecords', function(){
-    var percent = (this.recordsCount - 250)*100/250;
-
-    $('#stream_progress').text(percent + '%').attr('style', 'width: '+ percent +'%;');
-
-    if (percent == 100){
-      $('#stream_progress').parent().fadeOut(1000);
-    }
-  });
-
-  FJS.setStreaming({
-    data_url: 'data/stream_movies.json',
-    stream_after: 1,
-    batch_size: 50
-  });
 
 //  FJS.addCriteria({field: 'year', ele: '#year_filter', type: 'range', all: 'all'});
 //  FJS.addCriteria({field: 'rating', ele: '#rating_filter', type: 'range'});
 //  FJS.addCriteria({field: 'runtime', ele: '#runtime_filter', type: 'range'});
-  FJS.addCriteria({field: 'category', ele: '#genre_category input:checkbox'});
+  FJS.addCriteria({field: 'category', ele: '#category_criteria input:checkbox'});
 
 
 
