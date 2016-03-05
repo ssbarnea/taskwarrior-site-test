af7bbf53d80d8cc13a71b5d15ec5dfe66d64133f
Bruno Vernay <brunovern.a@gmail.com>
2016-03-05 21:24:36 +0100

    Create tools-data.js

---
+++ tools-data.js
@@ -0,0 +1,23 @@
+var tools = [
+  {
+    "name": "The Shawshank Redemption",
+    "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    "rating": 9.3,
+    "director": "Frank Darabont",
+    "year": 1994,
+    "stars": [
+      "Tim Robbins",
+      "Morgan Freeman",
+      "Bob Gunton"
+    ],
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  }
+  ]
