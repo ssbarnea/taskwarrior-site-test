8289831cae55e21c4a5f2e20c6badab0b004b27d
BrunoVernay <BrunoVern.a@gmail.com>
2016-03-06 15:48:14 +0100

    Improvement
    Change-Id: I2126e29be4ca11fab6432bc5c32b80124fc7dbbf

--- filter.js/index.js
+++ filter.js/index.js
@@ -2,11 +2,6 @@ $(document).ready(function(){
 
   initCriteria();
 
-  //NOTE: To append in different container
-  var appendToContainer = function(htmlele, record){
-    console.log(record)
-  };
-
   var FJS = FilterJS(tools, '#tools', {
     template: '#tool-template',
     search: {ele: '#searchbox', fields: ['name', 'description', 'author']},
@@ -15,7 +10,6 @@ $(document).ready(function(){
         $('#total_tools').text(result.length);
       }
     }
-    //appendToContainer: appendToContainer
   });
 
 
@@ -25,20 +19,19 @@ $(document).ready(function(){
 //  FJS.addCriteria({field: 'runtime', ele: '#runtime_filter', type: 'range'});
   FJS.addCriteria({field: 'category', ele: '#category_criteria input:checkbox'});
   FJS.addCriteria({field: 'obsolete', ele: '#obsolete_criteria input:checkbox'});
-  FJS.addCriteria({field: 'license', ele: '#license_criteria input:checkbox', all: 'all'});
+    FJS.addCriteria({field: 'theme', ele: '#theme_criteria input:checkbox', all: 'all'});
+  FJS.addCriteria({field: 'language', ele: '#language_criteria input:checkbox', all: 'all'});
 
 
 
   window.FJS = FJS;
+  FJS.filter();
+
 });
 
 function initCriteria(){
   $('#category_criteria :checkbox').prop('checked', true);
   $('#all_category').on('click', function(){
     $('#category_criteria :checkbox').prop('checked', $(this).is(':checked'));
-  });
-  $('#license_criteria :checkbox').prop('checked', true);
-  $('#all_license').on('click', function(){
-    $('#license_criteria :checkbox').prop('checked', $(this).is(':checked'));
   });
 }
